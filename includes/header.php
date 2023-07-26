8105bbcde8b6c38a68877e04f0cb42dd30fd7cf5
Anthony Davis <42490176+AnthDavis26@users.noreply.github.com>
2023-07-26 15:34:03 -0400

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Akshar:wght@600&display=swap">
+  <link rel="stylesheet" href="styles/style.css">
+  <link rel="icon" type="image/x-icon" href="/images/favicon.png">
+  <title>Anthony Davis' Projects</title>
+</head>
+
+<body>
+  <header>
+    <h1>Anthony Davis' Projects</h1>
+  </header>
