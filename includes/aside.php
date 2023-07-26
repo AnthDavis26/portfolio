c7dd9220f49c6dab9a3d49adfc1544d7be02eab8
Anthony Davis <42490176+AnthDavis26@users.noreply.github.com>
2023-07-26 15:35:11 -0400

    Create aside.php

---
+++ includes/aside.php
@@ -0,0 +1,6 @@
+<aside>
+  <figure>
+    <img src="images/portrait1.jpg"></img>
+    <figcaption>Katie Saunders (left) with Anthony Davis (right), married since 2023</figcaption>
+  </figure>
+</aside>
